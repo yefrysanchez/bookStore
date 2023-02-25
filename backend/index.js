0946c353b753f18ae4d6c3f424856bba3b2d54cc
Yefry Sanchez <yefrymsp@gmail.com>
2023-02-25 18:09:44 -0500

    db and client connected

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,44 @@
-import express from 'express'
-import mysql from 'mysql'
+import express from "express";
+import mysql from "mysql";
+import cors from 'cors'
 
-const app = express()
+const app = express();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "@Platanopower123",
-    database: "book"
-})
+  host: "localhost",
+  user: "root",
+  password: "@Platanopower01",
+  database: "book",
+});
 
-app.get("/", (req, res)=> {
-    res.send("<h1>Hello World</h1>")
-})
+app.use(express.json());
+app.use(cors());
 
-app.get("/books", (req, res)=> {
-    const q = "SELECT * FROM books"
-    db.query(q,(err, data) => {
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
 
-app.get("/*", (req, res)=> {
-    res.send("<h1>Page Not Found</h1>")
-})
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
+app.get("/*", (req, res) => {
+  res.send("<h1>Page Not Found</h1>");
+});
 
+app.post("/books", (req, res) => {
+  const q = " INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created successfully.");
+  });
+});
 
 app.listen(3000, () => {
-    console.log("Connected!")
-})
+  console.log("Connected!");
+});
---
+++ client/src/pages/Books.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/books");
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>
+        Yefry Book Shop
+      </h1>
+    </div>
+  );
+};
+
+export default Books;
